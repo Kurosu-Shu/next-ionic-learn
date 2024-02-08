5d352bb8499f8789b550d6e6f8396737e2a57c92
Si Thu htet <sithu.tgi90@gmail.com>
2024-02-09 00:14:56 +0630

    first commit

---
+++ app/db.ts
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'myStore_db'
+});
+
+export async function query(sql: any, values: any) {
+    const [rows, fields] = await connection.execute(sql, values);
+    return rows;
+}
